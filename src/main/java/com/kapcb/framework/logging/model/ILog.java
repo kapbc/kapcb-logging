adc89a726c4267f96ded7cc264cd134f4f5443f3
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-26 22:40:49 +0800

    add dependencies

---
+++ src/main/java/com/kapcb/framework/logging/model/ILog.java
@@ -0,0 +1,13 @@
+package com.kapcb.framework.logging.model;
+
+/**
+ * <a>Title: ILog </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ILog <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/25 22:30
+ */
+public class ILog {
+}
