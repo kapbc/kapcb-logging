c00a37467d2a19a6c3166582b2caf64b484b6552
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-03 08:32:52 +0800

    add spring expression language

--- src/main/java/com/kapcb/framework/logging/support/SpringElSupport.java
+++ src/main/java/com/kapcb/framework/logging/support/SpringElSupport.java
@@ -2,6 +2,7 @@ package com.kapcb.framework.logging.support;
 
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
@@ -22,6 +23,7 @@ import java.lang.reflect.Method;
  * @date 2021/11/26 22:42
  */
 @Slf4j
+@UtilityClass
 public class SpringElSupport {
 
     private static final SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
@@ -35,7 +37,7 @@ public class SpringElSupport {
                 for (int i = 0; i < parameterNames.length; i++) {
                     context.setVariable(parameterNames[i], arguments[i]);
                 }
-                return parseException(springEl).getValue(context, clazz);
+                return parseExpression(springEl).getValue(context, clazz);
             }
         } catch (Exception e) {
             log.error("parse spring expression error, error message is : {}", e.getMessage());
@@ -43,7 +45,7 @@ public class SpringElSupport {
         return null;
     }
 
-    private static Expression parseException(String springEl) {
+    private static Expression parseExpression(String springEl) {
         if (StrUtil.startWith(springEl, "#{") && StrUtil.endWith(springEl, "}")) {
             return spelExpressionParser.parseExpression(springEl, new TemplateParserContext());
         }
