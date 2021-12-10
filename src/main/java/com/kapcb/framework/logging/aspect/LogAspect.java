c0b2696da03664136485b4d6c1523ff4d499d4f7
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-10 22:04:00 +0800

    remove no use code

--- src/main/java/com/kapcb/framework/logging/aspect/LogAspect.java
+++ src/main/java/com/kapcb/framework/logging/aspect/LogAspect.java
@@ -1,18 +1,14 @@
 package com.kapcb.framework.logging.aspect;
 
-import com.kapcb.framework.logging.annotation.Logging;
 import com.kapcb.framework.logging.processor.LogProcessor;
-import com.kapcb.framework.logging.properties.LogInfoProperties;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 
 /**
  * <a>Title: LogAspect </a>
